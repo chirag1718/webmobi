bc84e4d28d1556f0a9180658c98fc423e1b8cc52
Chirag <chiragsonar1718@gmail.com>
2023-06-21 15:47:12 +0530

    feat: created navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import "./navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="navWrapper">
+      <div className="navContainer">
+        <div className="logo">Incubyte</div>
+        <div className="navItems">
+          <div className="items">
+            <p>Our Solution</p>
+            <p>Quick Turnaround</p>
+            <p>Quality Assurance</p>
+            <p>Our Advantages</p>
+            <p>Contact Us</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
